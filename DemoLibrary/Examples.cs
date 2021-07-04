502882df03ecec2c1a811117159d30f886d85dc7
Adithya Vijay K <adithyavj@gmail.com>
2021-07-04 16:11:23 +0530

    Initial commit - create new library project with classes for calculation, DataAccess, and Exception

---
+++ DemoLibrary/Examples.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public static class Examples
+    {
+        public static string ExampleLoadTextFile(string file)
+        {
+            if (file.Length < 10)
+            {
+                throw new System.IO.FileNotFoundException();
+            }
+            return "This file was loaded correctly";
+        }
+    }
+}
