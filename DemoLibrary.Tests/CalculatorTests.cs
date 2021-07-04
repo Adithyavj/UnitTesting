d5c9c7191e5cdaed3a41a164934a74ff57af5e58
Adithya Vijay K <adithyavj@gmail.com>
2021-07-04 17:43:44 +0530

    Add UnitTest for Calculator

---
+++ DemoLibrary.Tests/CalculatorTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoLibrary;
+using Xunit;
+
+namespace DemoLibrary.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Add_SimpleValuesShouldCalculate()
+        {
+            // General Layout of a Test
+            // 1. Arrange - Where we arrange our values and get things setup to run out test
+            // 2. Act - Do actual action that we are testing
+            // 3. Assert - This is what i expect compare actual value and expected value
+
+            // Arrange
+            double expected = 5;
+
+            // Act
+            double actual = Calculator.Add(2, 3);
+
+            // Assert
+            Assert.Equal(expected, actual);
+
+        }
+
+    }
+}
