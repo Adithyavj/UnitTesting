5c2b79936f16e1ac492496f16a73ee4d19dea070
Adithya Vijay K <adithyavj@gmail.com>
2021-07-04 19:43:39 +0530

    Changed Fact to Theory to pass multiple values in the test using InLineData

--- DemoLibrary.Tests/CalculatorTests.cs
+++ DemoLibrary.Tests/CalculatorTests.cs
@@ -10,8 +10,12 @@ namespace DemoLibrary.Tests
 {
     public class CalculatorTests
     {
-        [Fact]
-        public void Add_SimpleValuesShouldCalculate()
+        //[Fact]
+        [Theory]
+        [InlineData(4,3,7)]
+        [InlineData(21, 5.25, 26.25)]
+        [InlineData(double.MaxValue,5,double.MaxValue)]
+        public void Add_SimpleValuesShouldCalculate(double x, double y, double expected)
         {
             // General Layout of a Test
             // 1. Arrange - Where we arrange our values and get things setup to run out test
@@ -19,10 +23,9 @@ namespace DemoLibrary.Tests
             // 3. Assert - This is what i expect compare actual value and expected value
 
             // Arrange
-            double expected = 5;
 
             // Act
-            double actual = Calculator.Add(2, 3);
+            double actual = Calculator.Add(x, y);
 
             // Assert
             Assert.Equal(expected, actual);
