5324089367c4b60cb0bfe80dc039c84f5e33d14d
Adithya Vijay K <adithyavj@gmail.com>
2021-07-05 00:07:45 +0530

    Implement Single Responsibility Principle in DataAccess Class. Unit test does test on methods with single responsibility. Create unit test for DataAccess.

---
+++ DemoLibrary.Tests/DataAccessTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using DemoLibrary;
+using DemoLibrary.Models;
+
+namespace DemoLibrary.Tests
+{
+    public class DataAccessTests
+    {
+        [Fact]
+        public void AddPersonToPeopleList_ShouldWork()
+        {
+            PersonModel newPerson = new PersonModel { FirstName = "Adithya", LastName = "Vijay" };
+            List<PersonModel> people = new List<PersonModel>();
+
+            DataAccess.AddPersonToPeopleList(people, newPerson);
+        }
+    }
+}
