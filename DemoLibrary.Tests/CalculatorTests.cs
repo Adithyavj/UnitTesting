ac7ada4ff3df0fb88a9be4cafe438b0e1cdb47c7
Adithya Vijay K <adithyavj@gmail.com>
2021-07-04 19:56:11 +0530

    Add test for divide and dividebyzero

--- DemoLibrary.Tests/CalculatorTests.cs
+++ DemoLibrary.Tests/CalculatorTests.cs
@@ -32,5 +32,29 @@ namespace DemoLibrary.Tests
 
         }
 
+        [Theory]
+        [InlineData(8, 4, 2)]
+        public void Divide_SimpleValuesShouldCalculate(double x, double y, double expected)
+        {
+            // Arrange
+
+            // Act
+            double actual = Calculator.Divide(x, y);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Divide_DivideByZero()
+        {
+            // Arrange
+            double expected = 0;
+            // Act
+            double actual = Calculator.Divide(15, 0);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
     }
 }
